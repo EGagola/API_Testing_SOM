ab32f1d67335442b8346d74bd820f49b946fba03
Elliot Gagola <egagola@spartaglobal.com>
2018-11-28 10:39:22 +0000

    V.0.2 Added in services folder and files

---
+++ lib/postcodeio.rb
@@ -0,0 +1,15 @@
+# Include in the services
+require_relative 'services/single_postcode_service'
+require_relative 'services/multiple_postcodes_service'
+
+# Super class
+class Postcodesio
+
+  def single_postcode_service
+
+  end
+
+  def multiple_postcodes_service
+
+  end
+end
